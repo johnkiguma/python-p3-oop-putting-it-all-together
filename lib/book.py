181399d020f2c4c8106a821ed47f40082027f1ef
johnkiguma <john.kiguma@student.moringaschool.com>
2024-03-04 11:36:41 +0300

    completed assignmen

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
-
 class Book:
-    pass
-    
+    '''Book class'''
+
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...ye youre almost done !!")
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+from book import Book
+
+
+book = Book("wizard of oz", 272)
+
+
+print("Title:", book.title)
+print("Page Count:", book.page_count)
+
+book.turn_page()
+
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    '''Shoe class'''
+
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+from shoe import Shoe
+
+
+stan_smith = Shoe("lv red ", 9)
+
+print("Brand:", stan_smith.brand)
+print("Size:", stan_smith.size)
+print("Condition:", stan_smith.condition)
+
+stan_smith.cobble()
+
+
+print("Condition after cobbling:", stan_smith.condition)
